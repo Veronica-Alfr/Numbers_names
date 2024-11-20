830307d0bc510e1e9cac725cf5c27c8743dcb839
Veronica-Alfr <veagalves@gmail.com>
2024-11-20 20:43:18 -0300

    feat: created method POST for convert number into their name

--- nameNumbers.js
+++ nameNumbers.js
@@ -15,6 +15,25 @@ app.get("/", (req, res) => {
   res.send("Number to their name");
 });
 
+app.post("/convert", (req, res) => {
+    const { number } = req.body;
+  
+    if (number === undefined || typeof number !== "number") {
+      return res
+        .status(400)
+        .json({ error: "Invalid input, please provide a number." });
+    }
+  
+    try {
+        const numberConvertInWords = extenso(number);
+        res.json({ number: numberConvertInWords });
+      } catch (error) {
+        res
+          .status(500)
+          .json({ error: "Error converting number to your real name." });
+    }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://${host}:${process.env.PORT}`);
 });
